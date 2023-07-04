61c016618e1244252bf6df3c3070424136ffd6f9
Alina Akulina <akulina.alina1999@gmail.com>
2023-07-04 17:04:14 +0000

    =(

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js';
 import { loader } from './js/cat-api.js';
 
-const error = document.querySelector('.error');
+const errorMessange = document.querySelector('.error');
+
+let chosenBred = null;
+let breeds = [];
 
  fetchBreeds()
  .then(breeds => {
@@ -15,9 +18,11 @@ const error = document.querySelector('.error');
   });
 
   const breedSelect = document.querySelector('.breed-select');
-  breedSelect.addEventListener('change', function () {
-    const selectedBreedId = this.value;
+
+  breedSelect.addEventListener('change', event => {
+    const selectedBreedId = event.target.value;
     loader.style.display = 'block';
+
 
     fetchCatByBreed(selectedBreedId)
       .then(catData => {
@@ -29,6 +34,27 @@ const error = document.querySelector('.error');
         console.error(error);
       });
   });
+
+  
+  function createCatInfo(catData, id) {
+    const cat = catData[0];
+    const catBreed = getBreedById(id);
+    return `
+      <div class="cat-info-container">
+        <div class="cat-text">
+          <h2>${catBreed.name}</h2>
+          <p>${catBreed.temperament}</p>
+          <p>${catBreed.description}</p>
+        </div>
+        <div class="cat-image">
+          <img src="${cat.url}" alt="${catBreed.name}">
+        </div>
+      </div>
+    `;
+  }
+
+  
+ 
 
 
 
