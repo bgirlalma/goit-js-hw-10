8d868e55b42218555e6bd591c5a089322091ac4f
Alina Akulina <akulina.alina1999@gmail.com>
2023-07-05 16:07:09 +0000

    HOMEWORK №10

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchBreeds, fetchCatByBreed, loader} from './js/cat-api.js';
+import { fetchBreeds, fetchCatByBreed, loader } from './js/cat-api.js';
+import SlimSelect from 'slim-select';
 
-import SlimSelect from 'slim-select'
+let breeds = [];
 
 new SlimSelect({
   select: '#selectElement',
   allowDeselect: true,
   showFirstOption: false,
-})
+});
 
-const error = document.querySelector('.error');
-error.style.display = 'none'
 fetchBreeds()
-
   .then(breedsData => {
     loader.style.display = 'none';
     breeds = breedsData;
@@ -20,12 +18,12 @@ fetchBreeds()
     breedSelect.innerHTML = breeds
       .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
       .join('');
-      
   })
   .catch(error => {
     loader.style.display = 'none';
     console.error(error);
   });
+
 const breedSelect = document.querySelector('.breed-select');
 const catInfoDiv = document.querySelector('.cat-info');
 breedSelect.addEventListener('change', event => {
@@ -48,25 +46,19 @@ function getBreedById(id) {
   return breeds.find(breed => breed.id === id);
 }
 
-  function createCatInfo(catData, id) {
-    const cat = catData[0];
-    const catBreed = getBreedById(id);
-    return `
-      <div class="cat-info-container">
-        <div class="cat-text">
-          <h2>${catBreed.name}</h2>
-          <p>${catBreed.temperament}</p>
-          <p>${catBreed.description}</p>
-        </div>
-        <div class="cat-image">
-          <img src="${cat.url}" alt="${catBreed.name}" width="500">
-        </div>
+function createCatInfo(catData, id) {
+  const cat = catData[0];
+  const catBreed = getBreedById(id);
+  return `
+    <div class="cat-info-container">
+      <div class="cat-text">
+        <h2>${catBreed.name}</h2>
+        <p>${catBreed.temperament}</p>
+        <p>${catBreed.description}</p>
       </div>
-    `;
-  }
-
-  
- 
-
-
-
+      <div class="cat-image">
+        <img src="${cat.url}" alt="${catBreed.name}" width="500">
+      </div>
+    </div>
+  `;
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,26 +1,30 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_rGPKsQNFaacf8ZKwo4FD76u3IIWhtdVGl17jsI1TgH9EBdH5SU9vWjNnqx35oYJz";
+import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export const loader = document.querySelector('.loader');
 const BASE_URL = 'https://api.thecatapi.com/v1/';
-const API_KEY = 'live_rGPKsQNFaacf8ZKwo4FD76u3IIWhtdVGl17jsI1TgH9EBdH5SU9vWjNnqx35oYJz'
+const API_KEY =
+  'live_rGPKsQNFaacf8ZKwo4FD76u3IIWhtdVGl17jsI1TgH9EBdH5SU9vWjNnqx35oYJz';
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+export const loader = document.querySelector('.loader');
+
 function fetchBreeds() {
   loader.style.display = 'none';
- return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`).then(response => {
-  if(!response.ok){
-    Notify.failure(`${error.textContent}`);
-  }
-  return response.json()
- })
+  const url = `${BASE_URL}breeds`;
+  return axios.get(url).then(response => {
+    if (response.status < 200 || response.status >= 300) {
+      Notify.failure('Error fetching breeds');
+    }
+    return response.data;
+  });
 }
 
 function fetchCatByBreed(breedId) {
   loader.style.display = 'none';
-  const url = `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`;
-  return axios.get(url)
-  .then(response => response.data)
+  const url = `${BASE_URL}images/search?breed_ids=${breedId}`;
+  return axios.get(url).then(response => response.data);
 }
+
 export { fetchBreeds, fetchCatByBreed };
 
 
